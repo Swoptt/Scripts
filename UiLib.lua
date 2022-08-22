aa006a98fb375994a6dad1a594d9cc11572d64c9
Swoptt <109099365+Swoptt@users.noreply.github.com>
2022-08-22 22:52:55 +0100

    Create UiLib.lua

---
+++ UiLib.lua
@@ -0,0 +1,29 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until shared.GuiLibrary
+local GuiLibrary = shared.GuiLibrary
+local ScriptSettings = {}
+local UIS = game:GetService("UserInputService")
+local LIB = function(tab, argstable) 
+    return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
+end
+
+function notify(text)
+    local frame = GuiLibrary["CreateNotification"]("Notification", text, 5, "assets/WarningNotification.png")
+    frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 64, 64)
+end
+function boxnotify(text)
+    if messagebox then
+        messagebox(text, "Notification", 0)
+     end
+end
+
+local Name = LIB("Utility", {
+    Name = "Name",
+    Function = function(callback) 
+        if callback then
+		print("script")
+        end
+    end,
+    Default = false,
+    HoverText = "hover text"
+})
